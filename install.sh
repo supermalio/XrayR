72ca3181ff6e943e51513f43008457764ea78486
supermalio <72795376+supermalio@users.noreply.github.com>
2021-06-14 14:17:40 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,17 +6,14 @@ file="./config.yml"
 link="http://127.0.0.1:667"
 docker_compose_file="./docker-compose.yml"
 #输入信息
-read -p "请输入容器名：" docker_name
-read -p "请输入面板地址：" apihost
-read -p "请输入前端面板KEY："  apikey
 read -p "请输入节点ID：" nodeid
 
 
 #写入信息
-sed -i "s|$link|$apihost|" $file
-sed -i "s|123|$apikey|" $file
+sed -i "s|$link|https://gogo.bid|" $file
+sed -i "s|123|200423|" $file
 sed -i "s|41|$nodeid|" $file
-sed -i "s|HIM|$docker_name|" $docker_compose_file
+sed -i "s|HIM|super|" $docker_compose_file
 
 echo "配置完成"
 echo "正在启动XrayR Docker Compose" && docker-compose up -d
