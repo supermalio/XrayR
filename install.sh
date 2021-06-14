e7e610bb5bbffdddb5b227501ac938d089eb9b87
supermalio <72795376+supermalio@users.noreply.github.com>
2021-06-14 19:18:46 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+
+#输入信息
+read -p "请输入节点ID：" nodeid
+
 suijishu=`cat /dev/urandom | tr -dc '0-9a-z' | head -c 15`
 git clone https://github.com/HIM01/docker_XrayR /tmp/$suijishu && cd /tmp/$suijishu
 sleep 5s
 file="./config.yml"
 link="http://127.0.0.1:667"
 docker_compose_file="./docker-compose.yml"
-#输入信息
-read -p "请输入节点ID：" nodeid
 
 
 #写入信息
